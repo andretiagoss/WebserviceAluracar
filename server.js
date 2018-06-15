a3b3402392bb0aae0bef333c0edc3fcec18e1085
rcalazansn <rcalazansn@gmail.com>
2018-06-15 18:09:18 -0300

    teste2

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const ip = extractIpParameter();
 const port = process.env.PORT || 1337;
 
 app.set('ip', ip);
-app.set('porta', port);
 
 app.use(cors());
 app.use(express.static('public'));
@@ -19,8 +18,5 @@ app.use(bodyParser.json());
 require('./api')(app);
 
 
-app.listen(port,()=>{
-    console.log('IP:'+ ip + ' porta:'+ port);
-
-});
+app.listen(port);
 
