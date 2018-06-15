2cf6fb3d95bb0dcf9f7bce72d4f0bd82a256c30d
rcalazansn <rcalazansn@gmail.com>
2018-06-15 18:05:24 -0300

    teste

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const extractIpParameter = () =>
     process.argv[2] ? process.argv[2] : 'localhost';
 
 const ip = extractIpParameter();
+const port = process.env.PORT || 1337;
+
 app.set('ip', ip);
+app.set('porta', port);
 
 app.use(cors());
 app.use(express.static('public'));
@@ -15,7 +18,7 @@ app.use(bodyParser.json());
 
 require('./api')(app);
 
-const port = process.env.PORT || 1337;
+
 app.listen(port,()=>{
     console.log('IP:'+ ip + ' porta:'+ port);
 
