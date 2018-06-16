e0e5706ae463a54b5d9a6b39658dfe4ee92cbe8c
Andre Tiago dos Santos Silva <andretiagoss@gmail.com>
2018-06-15 23:28:23 -0300

    ajuste na porta do endereço de acesso ao serviço

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
     , cors = require('cors');
 
 const extractIpParameter = () => 
-    process.argv[2] ? process.argv[2] : 'localhost';
+    process.argv[2] ? process.argv[1] : 'localhost';
 
 const ip = extractIpParameter();
 app.set('ip', ip); 
